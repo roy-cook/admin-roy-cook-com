52c730717bf8f3cb1719096878c72a6f5ccfa250
liuheng <liuheng@yidian-inc.com>
2019-02-18 14:30:02 +0800

    change home page text

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'Hi, I am roy-cook, welcome to my world!';
+    ctx.body = 'Hi, I am roy, welcome to my world!';
   }
 }
 
