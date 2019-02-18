dcd31a9276ab60be5d337262d0d31894bec19e1c
liuheng <liuheng@yidian-inc.com>
2019-02-18 10:12:38 +0800

    add delpoy script deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+cd src/github.com/RoyLH/admin-roy-cook-com
+pwd
+ls
+docker stop egg-server
+docker rm egg-server
+docker rmi node/egg-server
+docker build -t node/egg-server .
+docker run -d --name egg-server -p 5000:7001 node/egg-server
